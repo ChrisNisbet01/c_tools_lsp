6099515e523a5d1b773d259d241049949b9be7e2
Chris Nisbet <chris@nisbet.kiwi.nz>
2026-05-28 09:44:36 +1200

    Initial import: copy rpc_server (MCP server) as starting point
    Copied from /home/chris/projects/rpc_server, an MCP protocol server
    written in C using libubox, json-c, and the easy_pc GDL grammar toolkit.

---
+++ src/server.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <easy_pc/easy_pc.h>
+#include <json-c/json.h>
+#include <libubox/list.h>
+#include <libubox/runqueue.h>
+#include <libubox/uloop.h>
+#include <stdio.h>
+
+typedef struct rpc_server_st rpc_server_st;
+
+typedef bool (*rpc_handler_fn)(rpc_server_st * svr, struct json_object * params, struct json_object * id);
+
+typedef struct rpc_method_st
+{
+    char * name;
+    rpc_handler_fn handler;
+} rpc_method_st;
+
+typedef struct rpc_method_registry_st
+{
+    rpc_method_st * methods;
+    size_t count;
+    size_t capacity;
+} rpc_method_registry_st;
+
+typedef struct rpc_server_st
+{
+    int in_fd;
+    int out_fd;
+
+    struct uloop_fd stdin_fd;
+    struct uloop_fd out_uloop_fd;
+    struct list_head write_queue;
+    struct runqueue tool_queue;
+
+    struct
+    {
+        struct uloop_fd fd;
+        int pipe[2];
+    } completion;
+
+    epc_parser_t * parser;
+    epc_parse_session_t session;
+
+    rpc_method_registry_st registry;
+
+    bool eof_reached;
+    int exit_code;
+} rpc_server_st;
+
+void rpc_server_register_method(rpc_server_st * svr, char const * name, rpc_handler_fn handler);
+
+void rpc_server_queue_response(rpc_server_st * svr, struct json_object * res);
+
+void run_server(rpc_server_st * svr, int const in_fd, int const out_fd);
